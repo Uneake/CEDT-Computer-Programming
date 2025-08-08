095ef7d7eff18f56a7d43bcb394d2f5e49efb409
Prince <147691194+Uneake@users.noreply.github.com>
2025-08-08 09:07:01 +0700

    Change variable name and add comments

--- 07-08-2025/MajorChange.cpp
+++ 07-08-2025/MajorChange.cpp
@@ -3,29 +3,29 @@
 std::string id[2];
 double GPAX[2];
 char cp[2],cal_1[2],cal_2[2];
-int choosen_one=-1,choose_count=0;
+int chosen_one=-1,choose_count=0;
 
 main(){
     for(int i=0;i<2;i++){
         std::cin>>id[i]>>GPAX[i]>>cp[i]>>cal_1[i]>>cal_2[i];
-        if(cp[i]=='A'&&(cal_1[i]<='C')&&(cal_2[i]<='C')){
+        if(cp[i]=='A'&&(cal_1[i]<='C')&&(cal_2[i]<='C')){ //use ASCII to compare (A<B<C<...<Z)
             if(i==0){
-                choosen_one=0;
+                chosen_one=0; //Choose first student (we will change the chosen one after comparing)
                 choose_count++;
             }
             else{
-                if(choosen_one==-1){
-                    choosen_one=1;
+                if(chosen_one==-1){ //check that we haven't chosen yet
+                    chosen_one=1; //then choose this guy
                     choose_count++;
                 }
                 else{
-                    if(GPAX[1]>GPAX[0])choosen_one=1;
+                    if(GPAX[1]>GPAX[0])chosen_one=1;
                     else if(GPAX[1]==GPAX[0]){
-                        if(cal_1[1]<cal_1[0])choosen_one=1;
+                        if(cal_1[1]<cal_1[0])chosen_one=1; //use ASCII to compare (A<B<C<...<Z)
                         else if(cal_1[1]==cal_1[0]){
-                            if(cal_2[1]<cal_2[0])choosen_one=1;
+                            if(cal_2[1]<cal_2[0])chosen_one=1; //use ASCII to compare (A<B<C<...<Z)
                             else if(cal_2[1]==cal_2[0]){
-                                choose_count++;
+                                choose_count++; //both students are chosen
                             }
                         }
                     }
@@ -34,6 +34,7 @@ main(){
         }
     }
     if(choose_count==2)std::cout<<"Both";
-    else if(choose_count==1)std::cout<<id[choosen_one];
+    else if(choose_count==1)std::cout<<id[chosen_one];
     else std::cout<<"None";
+
 }

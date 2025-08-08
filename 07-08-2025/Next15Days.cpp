f8a7d56715ca46657d35e969a1e6d234c1d2dea0
Prince <147691194+Uneake@users.noreply.github.com>
2025-08-08 09:09:14 +0700

    Add comments

--- 07-08-2025/Next15Days.cpp
+++ 07-08-2025/Next15Days.cpp
@@ -4,9 +4,12 @@ int day_in_month[]={31,28,31,30,31,30,31,31,30,31,30,31};
 int d,m,y;
 main(){
     std::cin>>d>>m>>y;
+
+    //check leap year
     if((y-543)%400==0||((y-543)%4==0&&(y-543)%100!=0)){
         day_in_month[1]++;
     }
+    
     d+=15;
     if(d>day_in_month[m-1]){
         d = d%day_in_month[m-1];
@@ -17,4 +20,5 @@ main(){
         }
     }
     std::cout<<d<<'/'<<m<<'/'<<y;
+
 }

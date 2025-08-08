5e3efe86f10d38b8d44dba550f28b474ee43b76c
Prince <147691194+Uneake@users.noreply.github.com>
2025-08-08 08:50:13 +0700

    Add comments

--- 07-08-2025/Biorythm.cpp
+++ 07-08-2025/Biorythm.cpp
@@ -11,15 +11,20 @@ main(){
     ans+=365*(y_2-y_1-1);
     if(y_1%400==0||(y_1%4==0&&y_1%100!=0))ch1=true;
     if(y_2%400==0||(y_2%4==0&&y_2%100!=0))ch2=true;
+    
+    //calculate red part
     for(int i=0;i<m_1-1;i++){
         if(i==1){
             if(ch1)ans-=1;
         }
         ans-=day_in_month[i];
     }
-    ans-=d_1-1;
+    ans-=d_1-1; // this means ans = ans - d_1 + 1
     if(ch1)ans+=366;
     else ans+=365;
+    //red part = 366 or 365 - DayOfYear of date + 1(1 is birthday)
+
+    //calculate blue part
     for(int i=0;i<m_2-1;i++){
         if(i==1){
             if(ch2)ans+=1;
@@ -27,5 +32,8 @@ main(){
         ans+=day_in_month[i];
     }
     ans+=d_2-1;
+    //blue part = DayOfYear of date - 1
+    
     std::cout<<ans<<' '<<round(sin(2*M_PI*ans/23.0)*100.0)/100.0<<' '<<round(sin(2*M_PI*ans/28.0)*100.0)/100.0<<' '<<round(sin(2*M_PI*ans/33.0)*100.0)/100.0;
+
 }
